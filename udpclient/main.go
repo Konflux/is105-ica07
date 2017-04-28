e8d65d15a1d9eadbc127cf6d73d8e74a9f3fb822
chinatsu <lolexplode@gmail.com>
2017-04-28 11:28:16 +0200

    Enkel UDP server og klient

--- udpclient/main.go
+++ udpclient/main.go
@@ -12,8 +12,10 @@ func check(err error) {
 }
 
 func main() {
+  // Vi åpner en enkel connection til localhost på port 16384
   conn, err := net.Dial("udp", "127.0.0.1:16384")
   check(err)
   defer conn.Close()
+  // Så sender vi en hemmelig melding.
   conn.Write([]byte("Møte Fr 5.5 14:45 Flåklypa"))
 }
--- udpserver/main.go
+++ udpserver/main.go
@@ -12,17 +12,24 @@ func check(err error) {
 }
 
 func main() {
-  port := "16384"
+  port := "16384" // Porten vi ønsker at serveren skal høre etter UDP-pakker
   log.Printf("Starting server, listening on port %s\n", port )
+  // Vi hører på alle innkommende forespørsler ved å si at vi hører på
+  // :16384.
   addr, err := net.ResolveUDPAddr("udp", ":" + port)
   check(err)
+  // Åpne en UDP socket
   sock, err := net.ListenUDP("udp", addr)
   check(err)
 
   for {
     buf := make([]byte, 1024)
+    // ReadFromUDP blokkerer tråden til en melding kommer inn
+    // og putter data inn i buf, samt returnerer en read length som vi bruker
+    // senere
     rlen, _, err := sock.ReadFromUDP(buf)
     check(err)
+    // Vi printer ut buffer fra 0 til read length, og ikke lengre enn det.
     log.Println(string(buf[0:rlen]))
   }
 }
